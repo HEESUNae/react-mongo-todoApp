96fb2df0eb794fc45687941fe6b9760f09eb9d72
황희선 <hwanghuiseon@hwanghuiseon-ui-MacBookPro.local>
2023-07-06 23:14:20 +0900

    fix: baseUrl 변경

--- src/api/response.js
+++ src/api/response.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosApi = axios.create({
-  baseURL: 'https://vercel.com/heesunae/react-mongo-todo-app',
+  baseURL: 'https://react-mongo-todo-app.vercel.app',
   timeout: 100,
 });
