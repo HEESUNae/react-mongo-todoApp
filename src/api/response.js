d24a36a014f84157b4f0c48709c7e13f272dcd0a
황희선 <hwanghuiseon@hwanghuiseon-ui-MacBookPro.local>
2023-07-07 04:23:24 +0900

    front bulid test

--- src/api/response.js
+++ src/api/response.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export const axiosApi = axios.create({
   // baseURL: 'https://react-mongo-todo-app.vercel.app',
-  baseURL: 'http://localhost:4000',
+  baseURL: 'https://react-mongo-todo-app-server.vercel.app',
   timeout: 5000,
 });
